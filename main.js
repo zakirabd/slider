7ad7714a077f79840458f5651a9bb644f6ff28a2
zakirabd <zakir_abdurahimov@mail.ru>
2022-03-09 18:28:38 +0400

    upt

---
+++ main.js
@@ -0,0 +1,18 @@
+// const slides = document.querySelectorAll('.slide');
+// let num = 0;
+// let active = 2;
+// setInterval(() => {
+//     if(num === 4){
+//         num = 0;
+//         active = 2
+//     }else{
+//         num = num+1;
+//         active = active +1;
+//     }
+//     console.log(active)
+//     for(const slide of slides ){
+//         slide.style.transform = `translateX(${600 - num*300 }px)`;
+//         slide.classList.remove('active')
+//     }
+//     slides[active].classList.add('active')
+// }, 3000)
